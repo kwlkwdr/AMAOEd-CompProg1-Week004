041afa8dd8231ad3044963e410fcff94b6f7f240
kwlkwdr <72850221+kwlkwdr@users.noreply.github.com>
2020-10-16 10:09:43 +0800

    Add files via upload

---
+++ hello2.cpp
@@ -0,0 +1,39 @@
+/* ****************************************************************
+ * @author: Ryan Jule Cruz
+ * @app name: Hello World
+ * @app desc: ITE6102 - Week 004
+ * @history:
+ *  - <2020/10/14> | HelloWorld
+ *      -- Write a program that displays “Hello” in one line, 
+ *         and “World” in another line using exactly two (2) COUT statements.
+ * ****************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    cout << "Hello\n";
+    cout << "World\n";
+    cout << "Hello\n";
+    cout << "World\n";
+    cout << "Hello\n";
+    cout << "World\n";
+    cout << "Hello\n";
+    cout << "World\n";
+    cout << "Hello\n";
+    cout << "World\n";
+   
+}
